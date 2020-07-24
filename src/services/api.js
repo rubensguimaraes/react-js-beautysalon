b9a69136b23683e86354cd8c434733e340c9bdb6
Rubens Guimarães <guimaraes.rubens@gmail.com>
2020-07-24 09:41:37 -0300

    Atualização para deploy

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://167.99.2.107:3333',
+  baseURL: 'https://api-beautysalon.rubensguimaraes.com',
+  // baseURL: 'http://167.99.2.107:3333',
 });
 
 export default api;
